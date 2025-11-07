9055869a5eda48630026a1c46f352134b3d865f8
sekharg62 <ghoshsekhar6296@gmail.com>
2025-11-07 17:21:29 +0530

    base url updated

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-export const BASE_URL = "http://localhost:5000/api/";
+//export const BASE_URL = "http://localhost:5000/api/";
 
-//export const BASE_URL = "https://api.makautstudents.help/api/";
+export const BASE_URL = "https://api.makautstudents.help/api/";
 
 interface ApiPayload {
   [key: string]: any;
